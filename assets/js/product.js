e8c0f5f1ccef5f59061489d20e387cfa4b7999ec
dremmc98 <dreammc98@gmail.com>
2023-02-21 11:49:37 +0700

    Cards grid and fixed filter

--- assets/js/product.js
+++ assets/js/product.js
@@ -9,15 +9,15 @@ if (products) {
     if (imageSwitchItems.length > 1) {
       imageSwitchItems.forEach((el, index) => {
         el.setAttribute('data-index', index);
-        imagePagination.innerHTML += `<li class="image-pagination__item ${index === 0 ? 'image-pagination--active' : ''} " data-index="${index}"></li>`
+        imagePagination.innerHTML += `<li class="image-pagination__item ${index === 0 ? 'image-pagination__item--active' : ''} " data-index="${index}"></li>`
         el.addEventListener('mouseenter', (e) => {
-          currentProduct.querySelectorAll('.image-pagination__item').forEach(el => { el.classList.remove('image-pagination--active') });
-          currentProduct.querySelector(`.image-pagination__item[data-index="${e.currentTarget.dataset.index}"]`).classList.add('image-pagination--active');
+          currentProduct.querySelectorAll('.image-pagination__item').forEach(el => { el.classList.remove('image-pagination__item--active') });
+          currentProduct.querySelector(`.image-pagination__item[data-index="${e.currentTarget.dataset.index}"]`).classList.add('image-pagination__item--active');
         });
 
         el.addEventListener('mouseleave', (e) => {
-          currentProduct.querySelectorAll('.image-pagination__item').forEach(el => { el.classList.remove('image-pagination--active') });
-          currentProduct.querySelector(`.image-pagination__item[data-index="${0}"]`).classList.add('image-pagination--active');
+          currentProduct.querySelectorAll('.image-pagination__item').forEach(el => { el.classList.remove('image-pagination__item--active') });
+          currentProduct.querySelector(`.image-pagination__item[data-index="${0}"]`).classList.add('image-pagination__item--active');
         });
 
       });
